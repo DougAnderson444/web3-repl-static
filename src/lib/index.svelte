019064fbdb75b1728a9d5a42d9bad16673d6433d
Doug <douganderson444@gmail.com>
2021-09-01 22:05:33 -0300

    move to components

---
+++ src/lib/index.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	// import Repl from '$lib/repl/Repl.svelte';
+	let mounted;
+	let Repl;
+
+	onMount(async () => {
+		const _r = await import('$lib/repl/Repl.svelte');
+		Repl = _r.default;
+		mounted = true;
+	});
+</script>
+
+<svelte:head>
+	<title>Web3 Tokenizer REPL</title>
+</svelte:head>
+{#if mounted}
+	<svelte:component this={Repl} />
+{:else}
+	Loading...
+{/if}
